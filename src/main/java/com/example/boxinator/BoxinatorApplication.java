6c998a7e332e9d85f773f784c56e15ecc5caf3b9
Aman Zadran <amanzadran9@gmail.com>
2020-11-30 15:06:42 +0100

    initial commit

---
+++ src/main/java/com/example/boxinator/BoxinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.boxinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boxinator/BoxinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boxinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
