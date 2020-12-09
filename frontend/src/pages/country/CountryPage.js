c9af04ffa0b28a528fb65e3ba21fda3b75a3c827
NORDIC\pkarim <paria.karim@gmail.com>
2020-12-09 18:31:05 +0100

    lillap feat(CountryPage): Added table for fetchCountries

--- frontend/src/pages/country/CountryPage.js
+++ frontend/src/pages/country/CountryPage.js
@@ -46,26 +46,39 @@ const CountryPage = () => {
     }
 
     useEffect( () => {
-        const fetchCountries = async () => {
-            setIsLoading(true);
-            let response = await getAllCountries();
-            let {data: savedCountries} = response.data;
-
-            savedCountries = savedCountries.map((country) => {
-
-                return {
-                    countryId: country.countryId,
-                    countryName: country.name,
-                    countryCode: country.countryCode,
-                    feeMultiplier: country.feeMultiplier,
-                };
-            });
-            setCountries(savedCountries);
-            setIsLoading(false);
-            console.log(savedCountries);
-        };
         fetchCountries();
     }, []);
+
+    const fetchCountries = async () => {
+        setIsLoading(true);
+        let response = await getAllCountries();
+        let {data: savedCountries} = response.data;
+
+        savedCountries = savedCountries.map((country) => {
+
+            return {
+                countryId: country.id,
+                countryName: country.name,
+                countryCode: country.countryCode,
+                feeMultiplier: country.feeMultiplier,
+            };
+        });
+        setCountries(savedCountries);
+        setIsLoading(false);
+        console.log(savedCountries);
+    };
+
+    const countryObjects = countries.map((entry, index) =>
+        <tr key={index}>
+            <td>{entry.countryId}</td>
+            <td>{entry.countryName}</td>
+            <td>{entry.countryCode}</td>
+            <td>{entry.feeMultiplier}</td>
+            <td>
+                <button className="btn btn-info btn-sm mt-0">Edit</button>
+            </td>
+        </tr>
+    );
 
     //button handle click
     //loading || result
@@ -76,35 +89,36 @@ const CountryPage = () => {
     return(
         <div>
             <h1>Shipping Countries</h1>
-            <div>
-
 
                 <div className="addCountryForm">
                     <form>
                         <div className="form-group">
                             <label htmlFor="countryName">Country name: </label>
                             <input type="text" className="form-control"
-                                   placeholder="country name" id="countryName" onChange={onCountryNameChanged}/>
+                                   placeholder="Country Name" id="countryName" onChange={onCountryNameChanged}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="countryCode">Country code: </label>
                             <input type="text" className="form-control"
-                                   placeholder="country code" id="countryCode" onChange={onCountryCodeChanged}/>
+                                   placeholder="Country Code" id="countryCode" onChange={onCountryCodeChanged}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="feeMultiplier">Fee Multiplier: </label>
                             <input type="text" className="form-control"
-                                   placeholder="feeMultiplier" id="feeMultiplier" onChange={onFeeMultiplierChanged}/>
+                                   placeholder="Fee Multiplier" id="feeMultiplier" onChange={onFeeMultiplierChanged}/>
                         </div>
-                        <button onClick={onAddCountryClicked} className="btn btn-info"> Add country </button>
+                        <button onClick={onAddCountryClicked} className="btn btn-info" type="button"> Add country </button>
                     </form>
                 </div>
-            </div>
+                <button onClick={fetchCountries} className="btn btn-info" type="button">Get all countries</button>
 
-            <button onClick={getAllCountries} className="btn btn-info">Get all countries</button>
 
             <div className="allCountriesTable">
-                <h3>All Countries</h3>
+                <div className="row">
+                    <h3>All Countries</h3>
+                    <button onClick={onAddCountryClicked} className="btn btn-info btn-sm mt-0" type="submit"> Add country </button>
+                </div>
+
                 <table className="table table-bordered">
                     <thead className="thead-light">
                     <tr>
@@ -112,9 +126,11 @@ const CountryPage = () => {
                         <th scope="col">Country Name</th>
                         <th scope="col">Country Code</th>
                         <th scope="col">Fee Multiplier</th>
+                        <th scope="col">Edit</th>
                     </tr>
                     </thead>
                     <tbody>
+                        {countryObjects}
                     </tbody>
                 </table>
             </div>
