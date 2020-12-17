12c3fe551e918148b43aabdb2451d7d240229e65
ludwigcarlsson <zhendiac@gmail.com>
2020-12-17 15:21:26 +0100

    feat,refactor(multiple files): more changes, see last commit for details

--- frontend/src/pages/admin/AdminMenu.js
+++ frontend/src/pages/admin/AdminMenu.js
@@ -1,33 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { Button } from "react-bootstrap";
 
 const AdminMenu = props => {
 
   const [showAccounts, setShowAccounts] = useState(false);
-  const [showCountries, setShowCountries] = useState(false);
   const [showShipments, setShowShipments] = useState(false);
+  const [showCountries, setShowCountries] = useState(false);
 
   const getAccountList = () => {
     setShowAccounts(!showAccounts);
     props.getDetails(showAccounts, "accounts");
   }
 
-  const getCountryList = () => {
-    setShowCountries(!showCountries);
-    props.getDetails(showCountries, "countries")
-  }
-  
   const getShipmentList = () => {
     setShowShipments(!showShipments);
     props.getDetails(showShipments, "shipments");
   }
 
+  const getCountryList = () => {
+    setShowCountries(!showCountries);
+    props.getDetails(showCountries, "countries")
+  }
+
   return (
     <>
-      <Button onClick={getShipmentList}>Shipments</Button>
-      <Button onClick={getCountryList}>Countries</Button>
-      <Button onClick={getAccountList}>User details</Button>
+      <button type="button" className="btn btn-link" onClick={getAccountList}>Customer Accounts</button>
+      <button type="button" className="btn btn-link" onClick={getShipmentList}>Shipments</button>
+      <button type="button" className="btn btn-link" onClick={getCountryList}>Shipping Countries</button>
     </>
   );
 };
