a5cf4615efb121acd0a6a633807296c227041844
Aman Zadran <amanzadran9@gmail.com>
2020-12-23 11:36:10 +0100

    chore:refactored read of config

--- src/main/java/com/example/boxinator/Utils/FirebaseConfig.java
+++ src/main/java/com/example/boxinator/Utils/FirebaseConfig.java
@@ -3,11 +3,13 @@ package com.example.boxinator.Utils;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
@@ -17,25 +19,15 @@ import java.nio.file.Files;
 @Configuration
 public class FirebaseConfig {
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @Value("${firebase_path}")
     private String firebasePath;
 
     @PostConstruct
     public void init() {
 
-        File file = null;
-        try {
-            file = ResourceUtils.getFile("classpath:service-account-file.json");
-            String content = new String(Files.readAllBytes(file.toPath()));
-            System.out.println(content);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//Read File Content
 
 
         System.out.println(firebasePath);
@@ -45,8 +37,8 @@ public class FirebaseConfig {
         InputStream serviceAccount =
                 null;
         try {
-            Resource resource = new ClassPathResource("classpath:service-account-file.json");
-            serviceAccount = resource.getInputStream();
+            ClassPathResource resource = new ClassPathResource("service-account-file.json");
+            serviceAccount= resource.getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
