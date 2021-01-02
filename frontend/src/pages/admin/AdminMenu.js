f986af0d1bc4ec4b2a69ee73d5a912787d35a22a
Aman Zadran <amanzadran9@gmail.com>
2021-01-02 14:41:00 +0100

    feat: styling/icons for admin nav

--- frontend/src/pages/admin/AdminMenu.js
+++ frontend/src/pages/admin/AdminMenu.js
@@ -2,8 +2,11 @@ import { Button } from "react-bootstrap";
 import React from "react";
 import { useState } from "react";
 
-const AdminMenu = props => {
+import Location from "../../assets/location.png";
+import Employee from "../../assets/employee.png";
+import Box from "../../assets/box.png";
 
+const AdminMenu = (props) => {
   const [showAccounts, setShowAccounts] = useState(false);
   const [showShipments, setShowShipments] = useState(false);
   const [showCountries, setShowCountries] = useState(false);
@@ -11,23 +14,40 @@ const AdminMenu = props => {
   const getAccountList = () => {
     setShowAccounts(!showAccounts);
     props.getDetails(showAccounts, "accounts");
-  }
+  };
 
   const getShipmentList = () => {
     setShowShipments(!showShipments);
     props.getDetails(showShipments, "shipments");
-  }
+  };
 
   const getCountryList = () => {
     setShowCountries(!showCountries);
-    props.getDetails(showCountries, "countries")
-  }
+    props.getDetails(showCountries, "countries");
+  };
 
   return (
     <>
-      <Button variant="link" onClick={getAccountList}>Customer Accounts</Button>
-      <Button variant="link" onClick={getShipmentList}>Shipments</Button>
-      <Button variant="link" onClick={getCountryList}>Shipping Countries</Button>
+      <div className="admin-navlinks">
+        <img src={Employee} alt="customers"></img>
+        <Button variant="link" onClick={getAccountList}>
+          Customer Accounts
+        </Button>
+      </div>
+      <div className="admin-navlinks">
+        <img src={Box} alt="shipments"></img>
+
+        <Button variant="link" onClick={getShipmentList}>
+          Shipments
+        </Button>
+      </div>
+      <div className="admin-navlinks">
+        <img src={Location} alt="location"></img>
+
+        <Button variant="link" onClick={getCountryList}>
+          Shipping Countries
+        </Button>
+      </div>
     </>
   );
 };
