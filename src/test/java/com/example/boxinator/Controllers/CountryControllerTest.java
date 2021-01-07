0b438b5d86f4fb96d126ed87fbaf10ad7c23e2bd
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2021-01-07 17:59:10 +0100

    See previous commit

--- src/test/java/com/example/boxinator/Controllers/CountryControllerTest.java
+++ src/test/java/com/example/boxinator/Controllers/CountryControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.boxinator.Controllers;
 
+import com.example.boxinator.FirebaseToken;
 import com.example.boxinator.Models.Country;
 import com.example.boxinator.Utils.CommonResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -26,10 +27,10 @@ private CountryController countryController;
     4. Refresh page
     5. Find the header "Name" and click on token?key=
     6. Copy the entire id_token
-    7.Paste into the string testToken below after the "Bearer ...".
+    7.Paste into the string testToken below after the "Bearer ...". in the class FirebaseToken.
 */
 
-    private final String testToken= "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImUwOGI0NzM0YjYxNmE0MWFhZmE5MmNlZTVjYzg3Yjc2MmRmNjRmYTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYm94aW5hdG9yLWZjZDRjIiwiYXVkIjoiYm94aW5hdG9yLWZjZDRjIiwiYXV0aF90aW1lIjoxNjA4MDQyNzQwLCJ1c2VyX2lkIjoiTGpQcjdXTjJWVGhiZzJ5NTZVSlBabXphWTNnMiIsInN1YiI6IkxqUHI3V04yVlRoYmcyeTU2VUpQWm16YVkzZzIiLCJpYXQiOjE2MTAwMjMyNDksImV4cCI6MTYxMDAyNjg0OSwiZW1haWwiOiJvc2Nhci5kYWhscXVpc3RAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsib3NjYXIuZGFobHF1aXN0QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIiwic2lnbl9pbl9zZWNvbmRfZmFjdG9yIjoicGhvbmUiLCJzZWNvbmRfZmFjdG9yX2lkZW50aWZpZXIiOiI2NjBhNTJiOS0xMWIyLTRiOWQtODRhNi0xZjU1MDBlYTQzMWYifX0.1iG57Vnh8PeZUWewFA8s4Fa-Sm1B7kvLyJE1HxNoc99bUGJUJc-1RiMNOYOhzxai_bOpY8_GA0XdS11Y9qO6S4U-6cbtOdTwMZb_Ma4jdGQlUGZ9S_OB9XnGrwja36LZnXU4tVEDHVVu4H6gPVLdtMbz_W42UTbh9z3bXOXLRIWlux2lxzCfhfsppGhD1wq2QCza-bKqHHfYbGgwnNVt_QnveZA08YJWXNVgC_tDFLpaiafeZ545GW9KYR1UiizFgwilhh84wHSPYEoGCwzheQ5ENlaFvFj4jIV_7MHNFvX70zv0wKiOySNEaZdKcL2FO4ke2K9JVAkU2lmiRe1CIw";
+    private final String testToken= FirebaseToken.token;
 
 
     @Test
@@ -54,16 +55,31 @@ private CountryController countryController;
     @Test
     @DisplayName("Testing_Get_All_Countries")
     void getAllCountries() {
-        
+
+        //Given
+        //When
+        ResponseEntity<CommonResponse> res = countryController.getAllCountries();
+        //Then
+        assertEquals(HttpStatus.OK,res.getStatusCode());
     }
 
     @Test
     @DisplayName("")
     void updateCountryById() {
+        //Given
+
+        //When
+
+        //Then
     }
 
     @Test
     @DisplayName("")
     void deleteCountryById() {
+        //Given
+
+        //When
+
+        //Then
     }
 }
